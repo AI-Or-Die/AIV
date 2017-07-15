05a49dc01588e9b883d0e6e4f867babf255451c5
Jared Jensen <j@redj.xyz>
2017-07-15 06:09:32 +0000

    Basic heading is working. I want to rearrange the code a bit and make the heading decision smarter, but it's working!

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 ./apriltags/build/bin/front_back_camera_demo -N 1 -n config.txt -W 640 -H 360 -O front.txt &
 ./apriltags/build/bin/front_back_camera_demo -N 2 -n config.txt -W 640 -H 360 -O back.txt &
+./apriltags/build/bin/get_heading front.txt back.txt heading.txt
 wait
