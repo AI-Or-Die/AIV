f904c24bb4ea0130710e5856533b87cbd781cfa7
Jared Jensen <j@redj.xyz>
2017-07-26 06:56:49 +0000

    Added conversion from heading to letters for transfer to board

--- ai.py
+++ ai.py
@@ -1,6 +1,8 @@
 import serial
 import sys
 
+h_fov = 78.0  # TODO: Read this in from config.txt and calculate real horizontal angle
+
 def main():
     if len(sys.argv) != 4:
         print("This requires 3 arguments: the front input file, the back input file, and the output file")
@@ -18,6 +20,28 @@ def main():
             for line in back_file:
                   back_heading = float(line)
             print(front_heading or back_heading) 
+        heading_string = degreesToMotorDirections(front_heading or back_heading)
+        print(heading_string)
+
+def degreesToMotorDirections(angle):
+    # Get speed between 0 and 25
+    normalized_angle = angle / h_fov
+    if normalized_angle < -1:
+        normalized_angle = -1
+    if normalized_angle > 1:
+        normalized_angle = 1
+
+    # Find alphanumeric letter
+    letter_number = abs(int(normalized_angle * 25))
+    
+    if angle < 0:
+        leftLetter = chr(ord('A') + letter_number)
+        rightLetter = chr(ord('a') + letter_number)
+    else:
+        leftLetter = chr(ord('a') + letter_number)
+        rightLetter = chr(ord('A') + letter_number)
+
+    return leftLetter + rightLetter
     
 
 def displayTTYSend(str1):
