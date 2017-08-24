249b268909e6d09e8a66725ae162a3989bfa85e6
Jared Jensen <j@redj.xyz>
2017-08-24 20:59:46 +0000

    Made tmux select upper pane when running headless

--- run_headless.sh
+++ run_headless.sh
@@ -8,6 +8,7 @@ tmux split-window -t aiv ./apriltags/build/bin/aiv_apriltag_detector -N 2 -n con
 #apr2=$!
 tmux split-window -t aiv python3 ai.py front.txt back.txt heading.txt
 tmux select-layout -t aiv main-horizontal
+tmux select-pane -U -t aiv
 #ai=$!
 #trap 'kill -TERM $apr1; kill -TERM $apr2; kill -TERM $ai' INT
 #wait
