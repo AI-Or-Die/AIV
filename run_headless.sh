72355c1ae52dd82b6b65a9acc8ddfd2ab43293ae
Jared Jensen <j@redj.xyz>
2017-08-24 20:52:12 +0000

    Changed run_headless to work in tmux

--- run_headless.sh
+++ run_headless.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-export DISPLAY=:0 # Show on the main display, not on ssh
-./apriltags/build/bin/aiv_apriltag_detector -N 1 -n config.txt -d & 
-apr1=$!
-./apriltags/build/bin/aiv_apriltag_detector -N 2 -n config.txt -d & 
-apr2=$!
-python3 ai.py front.txt back.txt heading.txt &
-ai=$!
-trap 'kill -TERM $apr1; kill -TERM $apr2; kill -TERM $ai' INT
-wait
+DIR=`dirname $0`
+cd ${DIR}
+tmux new-session -d -s aiv
+tmux split-window -t aiv ./apriltags/build/bin/aiv_apriltag_detector -N 1 -n config.txt -d
+#apr1=$!
+tmux split-window -t aiv ./apriltags/build/bin/aiv_apriltag_detector -N 2 -n config.txt -d
+#apr2=$!
+tmux split-window -t aiv python3 ai.py front.txt back.txt heading.txt
+tmux select-layout -t aiv main-horizontal
+#ai=$!
+#trap 'kill -TERM $apr1; kill -TERM $apr2; kill -TERM $ai' INT
+#wait
